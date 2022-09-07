10de765d23eff7ced5948d70c65ad48922809f2f
Samuel Dare-Owonibi <dareowonibisamuel@gmail.com>
2022-09-07 19:27:13 +0000

    starter kit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          <h2>Welcome To Oja</h2>
+          <h6>We are soon to launch</h6> 🚀
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
